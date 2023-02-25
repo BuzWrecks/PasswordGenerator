6b38983ce18edf05a5f9ce6848511ac167be1195
BuzWrecks <123795096+BuzWrecks@users.noreply.github.com>
2023-02-25 22:43:26 +1100

    confirm buttons working and printing to console

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,42 @@
 
-// The user clicks the 'generate' button
+// Testing the console
+console.log("test")
 
 // Assignment Code
+
+// The user clicks the 'generate' button
+
 var generateBtn = document.querySelector("#generate");
+var generatePassword
+
+// Event listener opens the confirm
+generateBtn.addEventListener("click", generatePassword);
+
+function confirmGenerate() {
+  var generatePassword
+  if (confirm("Do you want to generate a password today? Click 'OK' to proceed.") === true) {
+    // print to console
+    
+    console.log("Password Generator commenced!");
+    
+    var passwordLenth = prompt("Password Length: choose a number between 8 and 128.");
+  
+  //if (prompt(8~128) == true)
+    
+  } else {
+    // print to console
+    console.log("Password Generator cancelled!");
+  }
+}
+
+
+
+
+
+/*
+function passwordLength() {
+  var passwordLenth = prompt("Password Length: choose a number between 8 and 128.");
+}
 
 // Event listener runs the first prompt
 // Add event listener to generate button
@@ -10,7 +44,13 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", generatePassword);
 */
 
-// Write password to the #password input
+/*
+
+function passwordLength() {
+  var passwordLenth = prompt("Password Length: choose a number between 8 and 128.");
+}
+
+Write password to the #password input
 function writePassword() {
     var password = writePassword();
     var passwordText = document.querySelector("#password");
@@ -24,10 +64,10 @@ let passwordLenth = prompt("Password Length: choose a number between 8 and 128."
 // Run an error check for numbers < 8 and > 128. If selection is outside of stated parameters run "error = please choose a number between 8 and 128"
 
 // User selects which character types to use
-let includeNumbers = prompt("Do you want to include numbers in your password? Click 'Ok' to include, click 'cancel' to leave out.");
-let includeSpecialCharacters = prompt("Do you want to include special characters in your password? Click 'Ok' to include, click 'cancel' to leave out.");
-let includeUppercase = prompt("Do you want to include uppercase letters in your password? Click 'Ok' to include, click 'cancel' to leave out.");
-let includeLowercase = prompt("Do you want to include lowercase letters in your password? Click 'Ok' to include, click 'cancel' to leave out.");
+let includeNumbers = alert("Do you want to include numbers in your password? Click 'Ok' to include, click 'cancel' to leave out.");
+let includeSpecialCharacters = alert("Do you want to include special characters in your password? Click 'Ok' to include, click 'cancel' to leave out.");
+let includeUppercase = alert("Do you want to include uppercase letters in your password? Click 'Ok' to include, click 'cancel' to leave out.");
+let includeLowercase = alert("Do you want to include lowercase letters in your password? Click 'Ok' to include, click 'cancel' to leave out.");
 // user to select 'ok' to include, cancel to leave out
 /* run error check - user must select at least one character type - if error, 
 return "you did not select any characters. Retry?" If click 'ok' rerun from start, if cancel - end 
